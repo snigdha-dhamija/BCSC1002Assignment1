25d630dd538f20dd12ee40ef645c7bd1240acc98
snigdha-dhamija <67826444+snigdha-dhamija@users.noreply.github.com>
2020-08-30 20:04:20 +0530

    Update Library.java
    Adding parameterized and non parameterized constructors

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,25 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Snigdha Dhamija
+ *  Date: 30/08/20
+ *  Time: 7:57 PM
  *  File Name : Library.java
  * */
 package definitions;
 
 public class Library {
-}
+
+private static final int MAXIMUM_NUMBER_OF_BOOKS_IN_LIBRARY = 5;
+    private Book [] booksThatAreRecentlyAvailable;
+
+    // Parameterized Constructor
+    public Library(Book[] booksThatAreRecentlyAvailable) {
+        this.booksThatAreRecentlyAvailable = booksThatAreRecentlyAvailable;
+    }
+
+    // Non-Parameterized Constructor
+    public Library() {
+        this.booksThatAreRecentlyAvailable = new Book[MAXIMUM_NUMBER_OF_BOOKS_IN_LIBRARY];
+        for (int libraryIndex = 0; libraryIndex < MAXIMUM_NUMBER_OF_BOOKS_IN_LIBRARY; libraryIndex++) {
+            this.booksThatAreRecentlyAvailable[libraryIndex] = new Book();
+        }
+    }
