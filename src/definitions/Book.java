58040e9cfa027283318e174ff380ad3ed7a4e4bf
snigdha-dhamija <67826444+snigdha-dhamija@users.noreply.github.com>
2020-08-30 17:59:18 +0530

    Update Book.java
    Adding constructors

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -4,7 +4,26 @@
  *  Time: 3:49 PM
  *  File Name : Book.java
  * */
-package definitions;
+package definitions; 
+
+import java.util.Objects;
 
 public class Book {
-}
+
+    private String nameOfTheBook;
+    private String nameOfTheAuthorOfTheBook;
+    private String thirteenDigitISBNNumberOfTheBook;
+
+    // Parameterized Constructor
+    public Book(String nameOfTheBook, String nameOfTheAuthorOfTheBook, String thirteenDigitISBNNumberOfTheBook) {
+        this.nameOfTheBook = nameOfTheBook;
+        this.nameOfTheAuthorOfTheBook = nameOfTheAuthorOfTheBook;
+        this.thirteenDigitISBNNumberOfTheBook = thirteenDigitISBNNumberOfTheBook;
+    }
+
+    // Non-Parameterized Constructor
+    public Book() {
+        this.nameOfTheBook = null;
+        this.nameOfTheAuthorOfTheBook = null;
+        this.thirteenDigitISBNNumberOfTheBook = null;
+    }
