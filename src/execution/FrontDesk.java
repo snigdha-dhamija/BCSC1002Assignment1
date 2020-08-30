37a1c2cc86258b1a8bb60d88d7ba1dd6c1745519
snigdha-dhamija <67826444+snigdha-dhamija@users.noreply.github.com>
2020-08-30 21:38:29 +0530

    Update FrontDesk.java
    Defining class

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,13 +1,21 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Snigdha Dhamija
+ *  Date: 30/08/20
+ *  Time: 9:35 PM
  *  File Name : FrontDesk.java
  * */
 package execution;
+import definitions.Library;
+import definitions.Student;
 
+import java.util.Scanner;
 public class FrontDesk {
-    public static void main(String[] args) {
+    private static final int ISSUE_A_NEW_BOOK = 1;
+    private static final int RETURN_A_PREVIOUSLY_ISSUED_BOOK = 2;
+    private static final int SHOW_ALL_ISSUED_BOOKS = 3;
+    private static final int EXIT = 4;
+    
+public static void main(String[] args) {
 
-    }
-}
+
+   
